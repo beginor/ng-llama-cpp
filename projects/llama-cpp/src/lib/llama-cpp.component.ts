7caf4fbf14dba9411ae93a6f9cb4daa6a74a7269
beginor <beginor@qq.com>
2024-06-14 10:14:01 +0800

    init workspace with angular-cli

---
+++ projects/llama-cpp/src/lib/llama-cpp.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-llama-cpp',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      llama-cpp works!
+    </p>
+  `,
+  styles: ``
+})
+export class LlamaCppComponent {
+
+}
---
+++ projects/llama-cpp/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of llama-cpp
+ */
+
+export * from './lib/llama-cpp.service';
+export * from './lib/llama-cpp.component';
